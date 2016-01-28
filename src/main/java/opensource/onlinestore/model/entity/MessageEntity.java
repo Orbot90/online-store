fbbc5003c20ffc8da94c313846f4aab8f82c5e41
Maks <avto12@i.ua>
2016-01-28 22:22:35 +0200

    fixed all errors during deployment

--- src/main/java/opensource/onlinestore/model/entity/MessageEntity.java
+++ src/main/java/opensource/onlinestore/model/entity/MessageEntity.java
@@ -1,7 +1,16 @@
 package opensource.onlinestore.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Created by maks(avto12@i.ua) on 27.01.2016.
  */
-public class MessageEntity {
+@Entity
+@Table(name = "messages")
+public class MessageEntity extends BaseEntity{
+
+    @ManyToOne
+    UserEntity user;
 }
--- src/main/java/opensource/onlinestore/model/entity/OrderEntity.java
+++ src/main/java/opensource/onlinestore/model/entity/OrderEntity.java
@@ -1,7 +1,17 @@
 package opensource.onlinestore.model.entity;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  * Created by maks(avto12@i.ua) on 26.01.2016.
  */
+
+
+@Entity
+@Table(name = "orders")
 public class OrderEntity extends BaseEntity {
+
+    @ManyToOne
+    UserEntity user;
 }
